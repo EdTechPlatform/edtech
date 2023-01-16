4649453a8949bfa0fcc61ee69e552b46409bb683
Bhushan Nakade <82373018+Bhushannn@users.noreply.github.com>
2023-01-16 19:12:16 +0530

    Module creation done

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,9 @@ import TutoriaTrack from "./componets/tutorialTrack/index.js";
 // import Consult from "./componets/consult/index.js";
 import VideoPlayer from "./componets/videoplayer";
 import TestRoute from "./componets/testRoute"
-import TutorialPage from "./componets/tutorialTrack/tutorialPage.js"
+import TutorialPage from "./componets/tutorialTrack/tutorialPage.js";
+import ModuleVideoPage from "./componets/tutorialTrack/moduleVideoPage";
+
 
 function App() {
   return (
@@ -23,6 +25,7 @@ function App() {
         <Route path="/account/tutorial" element={<TutoriaTrack/>} />
         <Route path="/account/tutorial/tutorialPage" element={<TutorialPage/>} />
         <Route path="/account/testroute" element={<TestRoute/>} />
+        <Route path="/account/tutorial/tutorialPage/modulevideo" element={<ModuleVideoPage/>} />
         {/* <Route path="/account/tutorial/module" element={<Consult/>} /> */}
         <Route path="/account/tutorial/consult/course" element={<VideoPlayer/>} />
       </Routes>
