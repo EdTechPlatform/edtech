41d5e08eb10898163be3708ef2b981ad737f4481
Bhushan Nakade <82373018+Bhushannn@users.noreply.github.com>
2023-02-01 13:29:59 +0530

    css added and right click disabled

--- admin/src/componets/tutorialTrack/moduleVideoPage.js
+++ admin/src/componets/tutorialTrack/moduleVideoPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Modal from '../modal/videoUploader'
 import Nav from "../nav";
+import "./videoplayer.css";
 
 const ModuleVideoPage = () => {
   const location = useLocation();
@@ -31,35 +32,37 @@ const ModuleVideoPage = () => {
   return (
     <>
       <Nav />
+      <h2 className="text-capitalize">{datav.moduleName}</h2>
         {arr && arr.map((item) => {
           return (
             <>
-              <div className="main" key={item._id}>
-                <div className="text">
-                  <h4 className="border border-3 text-left" onClick={() => setToggle(item._id)}>{item.videoTitle} </h4>
+              <main onContextMenu={(e) => e.preventDefault()} className="container" key={item._id}>
+              <section className="main-video shadow-sm p-3 bg-white mb-5 rounded hover-shadow">
+              <h4 className="text-capitalize text-center" onClick={() => setToggle(item._id)}>{item.videoTitle.slice(0,item.videoTitle.length-4)} </h4>
                   {toggle === item._id ? (
                     <>
                       {/* <p>{videoTitle}</p> */}
                     </>
                   ) : null}
-                </div>
+                </section>
 
-                <div className="img">
+                <section className="video-playlist">
                   {toggle === item._id ? (
                     <>
                       {/* <p>{videoLink}</p> */}
-                      <iframe
-                        width="50%"
-                        height="50%"
+                      <video onContextMenu={(e) => e.preventDefault()}
+                        width="100%"
+                        height="100%"
                         src={item.videoLink}
-                        title="YouTube video player"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
-                      ></iframe>
+                        controls
+                        controlsList="nodownload"
+                      ></video>
                     </>
                   ) : null}
-                </div>
-              </div>
+                </section>
+              </main>
             </>
           );
         })}
