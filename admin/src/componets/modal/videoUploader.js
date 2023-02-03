062bda1c86cf5a6d6dda22dc274fa889969bdf2d
siddhi-indgeos <siddhiagrawal2106@gmail.com>
2023-02-03 21:27:24 +0530

    bug fixing

--- admin/src/componets/modal/videoUploader.js
+++ admin/src/componets/modal/videoUploader.js
@@ -14,7 +14,7 @@ function VideoUpload() {
     const portfolioSlug = location.state.portfolioSlug;
     const moduleNumber = location.state.moduleNumber;
     const [uploadButtonText, setUploadButtonText] = useState("Upload Video");
-    const [progress, setProgress] = useState(0);
+    const [progress, setProgress] = useState();
     const [file, setFile] = useState(null);
     // 
     const [show, setShow] = useState(false);
@@ -46,17 +46,19 @@ function VideoUpload() {
         );
         setUploadButtonText("Video Uploaded");
         const json = await response.json();
-        if (json.success === true) {
+        console.log(json.success)
+        
             setTimeout(() => {
                 toast.success("Video Uploaded Successfully", {
                     position: "top-center",
                 });
             }, 100);
             setTimeout(() => {
-                navigate("/account/tutorial/tutorialPage/modulevideo", { replace: true });
+                setShow(false);
+                // navigate("/account/tutorial/tutorialPage/modulevideo", { replace: true });
                 setUploadButtonText("Upload Video");
             }, 2000);
-        }
+        
     };
     return (
         <>
